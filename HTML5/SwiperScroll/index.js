0d1915f66c52a1b82925b529e6359f0fc9efd230
VHero <a772839689@gmail.com>
2015-09-08 16:49:45 +0800

    SwiperScroll

--- HTML5/SwiperScroll/index.js
+++ HTML5/SwiperScroll/index.js
@@ -1,21 +1,24 @@
 $(document).ready(function(){
-		// 图片的缩放比例控制
-    scaleW = window.innerWidth / 320;
-    scaleH = window.innerHeight / 480;
-    var resizes = document.querySelectorAll('.resize');
-    for (var j = 0; j < resizes.length; j++) {
-        resizes[j].style.width = parseInt(resizes[j].style.width) * scaleW + 'px';
-        resizes[j].style.height = parseInt(resizes[j].style.height) * scaleH + 'px';
-        resizes[j].style.top = parseInt(resizes[j].style.top) * scaleH + 'px';
-        resizes[j].style.left = parseInt(resizes[j].style.left) * scaleW + 'px';
-    }
-
-    var mySwiper = new Swiper('.swiper-container', {
+	// 图片的缩放比例控制,只能应用在行间样式
+	function scaleControl(){
+		scaleW = window.innerWidth / 320;
+	    scaleH = window.innerHeight / 480;
+	    var resizes = document.querySelectorAll('.resize');
+	    for (var j = 0; j < resizes.length; j++) {
+	        resizes[j].style.width = parseInt(resizes[j].style.width) * scaleW + 'px';
+	        resizes[j].style.height = parseInt(resizes[j].style.height) * scaleH + 'px';
+	        resizes[j].style.top = parseInt(resizes[j].style.top) * scaleH + 'px';
+	        resizes[j].style.left = parseInt(resizes[j].style.left) * scaleW + 'px';
+	    }
+	}
+	// swiper的初始化以及swiper的一些函数的应用
+	function swiperInit(){
+		    var mySwiper = new Swiper('.swiper-container', {
             direction: 'vertical',
             pagination: '.swiper-pagination',
             mousewheelControl: true,
             watchSlidesProgress: true,
-
+            paginationClickable:true,
             onImagesReady: function(swiper){
      			 $('#loader').css('display','none');
     		},
@@ -33,4 +36,23 @@ $(document).ready(function(){
                 }
             },
         });
+	}
+	// 控制document的字体大小，应用rem这个单位可以让字体自适应尺寸
+	function sizeControl(doc, win){
+		var docEl = doc.documentElement,
+		resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+		recalc = function () {
+		var clientWidth = docEl.clientWidth;
+		if (!clientWidth) return;
+			docEl.style.fontSize = 20 * (clientWidth / 320) + 'px';
+		};
+		
+		if (!doc.addEventListener) return;
+			win.addEventListener(resizeEvt, recalc, false);
+			doc.addEventListener('DOMContentLoaded', recalc, false);
+		}
+	sizeControl(document,window);
+	scaleControl();
+	swiperInit();
+
 });
